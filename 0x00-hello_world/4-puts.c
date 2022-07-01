e9e59d9155a5c6e09ca0a3a978bd8d3afced873d
Katlegomad <katlego.madube@gmail.com>
2022-07-01 15:57:27 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,19 @@
-#include <stdio.h>/**
+#include <stdio.h>
 
-		     * main - Entry point
-		     *
-		     *  *
-		     *
-		     *   * Return: Always 0 (Success)
-		     *
-		     *    */
+/**
+ *
+ * * main-Entry point
+ *
+ * * Return: 0 if successful
+ *
+ * */
 
 int main(void)
 
 {
 
-	        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-		        return (0);
+	return (0);
 
-}			
+}
