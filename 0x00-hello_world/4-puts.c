dd9396fb735012e8bae74449dac73525e0327ce1
Katlegomad <katlego.madube@gmail.com>
2022-07-03 07:58:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
-
-/**
- *
- * * main-Entry point
- *
- * * Return: 0 if successful
- *
- * */
-
-int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
-}
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    getchar();
+    return 0;
+}    
