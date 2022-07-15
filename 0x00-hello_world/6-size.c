d09891e88ed15103b65a943f9eb2255db81c79f8
Katlegomad <katlego.madube@gmail.com>
2022-07-15 06:55:58 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of float: %zu bytes\n", sizeof(float));
-return (0);
+Return (0);
 }
