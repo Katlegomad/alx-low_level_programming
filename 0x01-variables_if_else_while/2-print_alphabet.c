5ca1d1c733fbd98313242ebd000c4c784207e5f3
Katlegomad <katlego.madube@gmail.com>
2022-07-16 07:03:57 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Prints alpha in lowercase
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	char ch = start;
+
+	while(ch <= end)
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	return (0);
+}
