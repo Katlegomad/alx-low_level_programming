e0cd5be45864b33fa721089de9b97e59ec8a087c
Katlegomad <katlego.madube@gmail.com>
2022-07-16 08:06:33 -0700

    alpha_lower_and_upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Prints alpha in lower and uppercase
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	char ch = start;
+
+	while (ch <= end)
+	{
+		putchar(ch);
+		ch++;
+
+	}
+{
+	char start = 'A';
+	char end = 'z';
+
+	char ch = start;
+
+	while (ch <= end)
+	{
+		putchar(ch);
+		ch++;
+
+	}
+}
+		putchar('\n')
+	
+	return (0);
+}
