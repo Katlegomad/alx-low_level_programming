9a5ea1a10a4ff92733e24ec98fd37b74b1697925
Katlegomad <katlego.madube@gmail.com>
2022-07-16 23:07:41 -0700

    alphaexcl

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Print all alpha exclude e and q
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char alpha ='a';
+
+	while (alpha <= 'z')
+	
+	{
+		putchar(alpha);
+		alpha++;
+	if (alpha == 'e' || alpha == 'q')
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
