d767d6a0f88fd202f74528f467d739e8a6dcf0f8
Katlegomad <katlego.madube@gmail.com>
2022-07-17 00:06:13 -0700

    printno

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Prints all single digit nos
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i != 10; i++)
+		{
+			printf("%d" ,i);
+		}
+	printf("\n");
+	return (0);
+}
